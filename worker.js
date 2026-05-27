1528513650352ad2b8f26dd68e7541baf1c8eeb6
emretprk <emretprk@gmail.com>
2026-05-27 12:15:12 +0300

    Fix sitemap content-type header

--- worker.js
+++ worker.js
@@ -1,5 +1,22 @@
 export default {
   async fetch(request, env) {
-    return env.ASSETS.fetch(request);
+    const url = new URL(request.url);
+    const response = await env.ASSETS.fetch(request);
+    
+    // Fix content-type for sitemap and robots
+    if (url.pathname === '/sitemap.xml') {
+      return new Response(response.body, {
+        headers: {
+          'Content-Type': 'application/xml',
+          'Cache-Control': 'public, max-age=3600',
+        }
+      });
+    }
+    if (url.pathname === '/robots.txt') {
+      return new Response(response.body, {
+        headers: { 'Content-Type': 'text/plain' }
+      });
+    }
+    return response;
   }
 };
